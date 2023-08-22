c5b3ab411b35f3172b9ef94b0064535b67343bc5
Antonio_Macek <antoniomacek3@gmail.com>
2023-08-22 15:37:28 +0200

    getStringFromList updated and changed the name of parameter in static log

--- src/Modules/utils.js
+++ src/Modules/utils.js
@@ -1,29 +1,27 @@
 class Utils {
   static getStringFromList(str, idx) {
     // This function returns a string from a comma separated list in str (example: "Piano, Drums, Guitar").
-    let result = null; // The variable is storing the name with index(if available), if not it is returning an empty string.
-    const items = str.split(",").map((item) => item.trim());
-    // When idx is 0, the first string is returned.
+    let result = ""; // The variable is storing the name with index(if available), if not it is returning an empty string.
+    const items = str.split(",");
     if (idx >= 0 && idx < items.length) {
-      result = items[idx];
-    } // When idx is invalid, an empty string is returned.
-    else {
+      result = items[idx].trim();
+    } else {
       result = "";
     }
 
     return result;
   }
 
-  static log(str, mode = 0) {
+  static log(msg, mode = 0) {
     // mode
     // 0 = console
     // 1 = window
     switch (mode) {
       case 0:
-        console.log(str);
+        console.log(msg);
         break;
       case 1:
-        window.alert(str);
+        window.alert(msg);
         break;
       default:
         break;
