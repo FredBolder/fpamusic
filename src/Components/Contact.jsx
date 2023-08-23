94d58757662e666cda77619bdd036b2cd987e282
priyanka-mehta02 <122629880+priyanka-mehta02@users.noreply.github.com>
2023-08-23 13:52:17 +0200

    loginpage

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,17 +1,20 @@
-
-import React from 'react';
-import { LoginContext } from './Contexts/LoginContexts'
-import { useContext } from 'react';
+import React from "react";
+import { LoginContext } from "./Contexts/LoginContexts";
+import { useContext } from "react";
 
 function Contact() {
-    const {setshowContact} = useContext(LoginContext);
+  const { setshowContact } = useContext(LoginContext);
   return (
     <div>
-       
-    <button onClick={()=>{setshowContact(true)}} >Contact</button>
-            
-        </div>
-  )
+      <button
+        onClick={() => {
+          setshowContact(true);
+        }}
+      >
+        Contact
+      </button>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,64 @@
-import React, { useContext } from 'react'
-import {useState} from 'react'
-import { LoginContext } from '../Context/LoginContext';
+import React, { useContext } from "react";
+import { LoginContext } from "../Context/LoginContext";
+import logofpa from "../Images/logofpa.png";
+
 function Login() {
-    const {setUsername ,setshowProfile} = useContext(LoginContext);
-   
+  const { setUsername, setshowProfile } = useContext(LoginContext);
+
   return (
+    <>
+      {""}
+      <div
+        class="position-relative"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "absolute",
+          backgroundImage: `url(${require("../Images/hero_background.jpg")})`,
+        }}
+      >
+        <h1>
+          <img
+            class="mt-1 p-1 position-absolute fixed top-0 start-0"
+            src={logofpa}
+            alt="Logo"
+            height="270"
+            width="270"
+          />
+        </h1>
+        <br></br>
+        <br></br>
+        <h2 class="text-light  fst-italic mx-auto p-2 position-absolute position-absolute bottom- 0  end-50">
+          "At FPAMusic",we’re making Music for Life"
+        </h2>
 
-    <> <h1>FPAMusic</h1>
-    <p>“We are delighted that you have decided to join us"....Welcome to our FPAmusic page</p>
- Please Enter your Name:
-    <input type ="text" placeholder ="username" onChange={(e)=>setUsername(e.target.value)}></input><hr/>
-    
-    <button onClick={()=>{setshowProfile(true)}} >Get started </button>
-    <p> info..........
-
-    </p>
-
+        <p class="text-light  fst-italic mb-1 p-2 position-absolute position-absolute position-absolute p top-50 start-50 translate-middle">
+          Please Enter your Name:-
+          <br></br>
+          <br></br>
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          
+         <br></br>
+          <br></br>
+          <button
+            className="btn btn-light"
+            onClick={() => {
+              setshowProfile(true);
+            }}
+          >
+      
+            Get started{" "}
+          </button>
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
