419a72852e0de4f60faba4c04299cb0a244a81a9
Antonio_Macek <antoniomacek3@gmail.com>
2023-08-29 15:45:35 +0200

    IntrumentPage_only_needs_fine_tuning_:)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import StartPage from "./Components/StartPage";
 import MainPage from "./Components/MainPage";
 import AboutPage from "./Components/AboutPage";
 import InstrumentsPage from "./Components/InstrumentsPage";
+import InstrumentPage from "./Components/InstrumentPage";
 import ContactPage from "./Components/ContactPage";
 import PageNotFound from "./Components/PageNotFound";
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/main" exact element={<MainPage/>} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/instruments" element={<InstrumentsPage />} />
+          <Route path="/instrument/:name" element={<InstrumentPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
@@ -24,3 +26,4 @@ function App() {
 }
 
 export default App;
+ 
--- src/Components/InstrumentPage.jsx
+++ src/Components/InstrumentPage.jsx
@@ -1,20 +1,133 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 import Logo from "./Logo";
 import NavBar from "./NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/style.css";
+import { Instruments } from "../Modules/instruments";
+import { Instrument } from "../Modules/instrument";
+import { Utils } from "../Modules/utils";
+import imgDarbuka from "../Images/darbuka_1.jpg";
+import imgDrums from "../Images/drums_1.jpg";
+import imgKalimba from "../Images/kalimba_1.jpg";
+import imgPiano from "../Images/piano_1.jpg";
+import imgRecorder from "../Images/recorder_1.jpg";
+import imgTongueDrum from "../Images/tongue_drum_1.jpg";
+import imgVoice from "../Images/voice_1.jpg";
 
 function InstrumentPage() {
+  const instruments = new Instruments();
+  const instrumentName = useParams().name;
+  const info = instruments.getInfo(instrumentName);
+  const linkCategories = instruments.getLinkCategories(info.links);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  function selectCategory(category) {
+    setSelectedCategory(category === selectedCategory ? null : category);
+  }
+
+  function getImage(instrument) {
+    let result = null;
+    const instrumentNameLower = instrument.toLowerCase();
+    switch (instrumentNameLower) {
+      case "darbuka":
+        result = imgDarbuka;
+        break;
+      case "drums":
+        result = imgDrums;
+        break;
+      case "kalimba":
+        result = imgKalimba;
+        break;
+      case "piano":
+        result = imgPiano;
+        break;
+      case "recorder":
+        result = imgRecorder;
+        break;
+      case "tongue drum":
+        result = imgTongueDrum;
+        break;
+      case "voice":
+        result = imgVoice;
+        break;
+      default:
+        result = null;
+        break;
+    }
+    return result;
+  }
   return (
     <div className="bg-dark text-light">
       <header className="d-flex justify-content-start">
         <Logo />
         <div>
           <NavBar />
-          <h1>Instrument</h1>
+          <h1 className="mt-5">{Utils.capitalize(instrumentName)}</h1>
         </div>
       </header>
+      <main className="container  mt-5 mb-5">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 rounded-4 shadow-lg p-4">
+            <img
+              src={getImage(instrumentName)}
+              alt={instrumentName}
+              className="img-fluid mb-4 rounded-4"
+            />
+            <h3>Description</h3>
+            <p className="fs-5 ">{info.generalInfo}</p>
+            <div>
+              <h3 className="mb-0">Resources</h3>
+              <p className="text-secondary fs-6">
+                (please choose the category below)
+              </p>
+              {linkCategories.map((category, index) => (
+                <div key={index}>
+                  <a
+                    className="text-decoration-none navlink text-capitalize fw-bold fs-4"
+                    onClick={() => selectCategory(category)}
+                  >
+                    {Utils.capitalize(category)}
+                  </a>
+                  {selectedCategory === category && (
+                    <ul className=" ml-0 p-0 text-white">
+                      {info.links[category].map((linkWithDesc, linkIndex) => {
+                        const link = Utils.getStringFromList(linkWithDesc, 0);
+                        const description = Utils.getStringFromList(
+                          linkWithDesc,
+                          1
+                        );
+                        return (
+                          <li
+                            className="d-flex align-items-center justify-content-start "
+                            key={linkIndex}
+                          >
+                            <a
+                              className="text-decoration-none fs-5"
+                              href={link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {link}
+                            </a>
+                            <p className="text-white mt-3 ms-3 fs-5">
+                              {Utils.capitalize(description.trim())}
+                            </p>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
 
-export default Instrument;
+export default InstrumentPage;
