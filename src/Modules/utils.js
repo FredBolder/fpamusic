6b92dac575fdb061e7213260b04640753fc6b87e
Fred Bolder <fgh.bolder@gmail.com>
2023-08-31 14:03:38 +0200

    sounds

--- src/Modules/utils.js
+++ src/Modules/utils.js
@@ -31,6 +31,35 @@ class Utils {
     }
   }
 
+  static getCaretPosition(editableDiv) {
+    // https://stackoverflow.com/questions/3972014/get-contenteditable-caret-position
+    // Changed the code to the new JavaScript standard
+    let caretPos = 0;
+    let sel;
+    let range;
+
+    if (window.getSelection) {
+      sel = window.getSelection();
+      if (sel.rangeCount) {
+        range = sel.getRangeAt(0);
+        if (range.commonAncestorContainer.parentNode === editableDiv) {
+          caretPos = range.endOffset;
+        }
+      }
+    } else if (document.selection && document.selection.createRange) {
+      range = document.selection.createRange();
+      if (range.parentElement() === editableDiv) {
+        let tempEl = document.createElement("span");
+        editableDiv.insertBefore(tempEl, editableDiv.firstChild);
+        let tempRange = range.duplicate();
+        tempRange.moveToElementText(tempEl);
+        tempRange.setEndPoint("EndToEnd", range);
+        caretPos = tempRange.text.length;
+      }
+    }
+    return caretPos;
+  }
+
   static getStringFromList(str, idx) {
     // This function returns a string from a comma separated list in str (example: "Piano, Drums, Guitar").
     let result = ""; // The variable is storing the name with index(if available), if not it is returning an empty string.
@@ -42,6 +71,11 @@ class Utils {
     }
 
     return result;
+  }
+
+  static getWordBetweenBrackets(text, position) {
+
+    return "tek";
   }
 
   static log(msg, mode = 0) {
@@ -72,7 +106,8 @@ class Utils {
 
   static removeChars(s, c) {
     // This function removes the characters that are in the string c from the string s
-    // Example: removeChars("(Test)", "()") returns Test
+    // Example 1: removeChars("(Test)", "()") returns Test
+    // Example 2: removeChars("*** Priyanka ***", " *") returns Priyanka
     return "";
   }
 
